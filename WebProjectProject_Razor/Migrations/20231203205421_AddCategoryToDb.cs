c95fc5c04a1507a604572bb9f5c0250ca9963e7f
LUKE,OLIVER,JOHN ATKINSON <lukeatkinson3@gmail.com>
2023-12-06 19:56:49 +0000

    Creating Same Project with Razor pages instead

---
+++ WebProjectProject_Razor/Migrations/20231203205421_AddCategoryToDb.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace WebProjectProject_Razor.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCategoryToDb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: false),
+                    DisplayOrder = table.Column<int>(type: "int", nullable: false),
+                    CreatedDateTime = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "Id", "CreatedDateTime", "DisplayOrder", "Name" },
+                values: new object[,]
+                {
+                    { 1, new DateTime(2023, 12, 3, 20, 54, 20, 965, DateTimeKind.Local).AddTicks(207), 1, "Action" },
+                    { 2, new DateTime(2023, 12, 3, 20, 54, 20, 965, DateTimeKind.Local).AddTicks(266), 2, "SciFi" },
+                    { 3, new DateTime(2023, 12, 3, 20, 54, 20, 965, DateTimeKind.Local).AddTicks(268), 3, "History" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Categories");
+        }
+    }
+}
