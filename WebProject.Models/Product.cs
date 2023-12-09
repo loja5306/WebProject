bba50bb3a3488089b1cbb58f5a00b29a546e6f3d
LUKE,OLIVER,JOHN ATKINSON <lukeatkinson3@gmail.com>
2023-12-09 21:18:26 +0000

    Adding Product Controller & Views

--- WebProject.Models/Product.cs
+++ WebProject.Models/Product.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -35,6 +36,10 @@ namespace WebProject.Models
         [DisplayName("Price for 100+")]
         [Range(1, 1000)]
         public double Price100 { get; set; }
+
+        public int CategoryID { get; set; }
+        [ForeignKey("CategoryID")]
+        public Category Category { get; set; }
 
     }
 }
