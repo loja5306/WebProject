a546afdf13e18f65c3a695530dc094635dd74538
LUKE,OLIVER,JOHN ATKINSON <lukeatkinson3@gmail.com>
2023-12-09 20:34:11 +0000

    Creating Product Table, Seeding Table & Creating Repository

--- WebProject.DataAccess/Repository/IRepository/IUnitOfWork.cs
+++ WebProject.DataAccess/Repository/IRepository/IUnitOfWork.cs
@@ -9,6 +9,7 @@ namespace WebProject.DataAccess.Repository.IRepository
     public interface IUnitOfWork
     {
         ICategoryRepository Category { get; }
+        IProductRepository Product { get; }
 
         void Save();
     }
