93ef3b080da45abed1be673f28cdc28e359c35e2
LUKE,OLIVER,JOHN ATKINSON <lukeatkinson3@gmail.com>
2023-12-16 19:48:20 +0000

    Editing Home Page

--- WebProjectProject/Areas/Customer/Controllers/HomeController.cs
+++ WebProjectProject/Areas/Customer/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using WebProject.DataAccess.Repository.IRepository;
 using WebProject.Models;
 
 namespace WebProject.Areas.Customer.Controllers
@@ -8,15 +9,18 @@ namespace WebProject.Areas.Customer.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IUnitOfWork _unitOfWork;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
         {
             _logger = logger;
+            _unitOfWork = unitOfWork;
         }
 
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Product> productList = _unitOfWork.Product.GetAll(includeProperties: "Category");
+            return View(productList);
         }
 
         public IActionResult Privacy()
