0a6eb9c3ef3059c700040205e280ab1cb5a7b88e
LUKE,OLIVER,JOHN ATKINSON <lukeatkinson3@gmail.com>
2023-12-29 23:15:24 +0000

    Updating ShoppingCart Model & DB Table

---
+++ WebProject.DataAccess/Migrations/20231229230907_UpdateShoppingCartTable.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebProject.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateShoppingCartTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ApplicationUserId",
+                table: "ShoppingCarts",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedDateTime",
+                value: new DateTime(2023, 12, 29, 23, 9, 6, 462, DateTimeKind.Local).AddTicks(8976));
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedDateTime",
+                value: new DateTime(2023, 12, 29, 23, 9, 6, 462, DateTimeKind.Local).AddTicks(9029));
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreatedDateTime",
+                value: new DateTime(2023, 12, 29, 23, 9, 6, 462, DateTimeKind.Local).AddTicks(9031));
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ShoppingCarts_ApplicationUserId",
+                table: "ShoppingCarts",
+                column: "ApplicationUserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ShoppingCarts_ProductId",
+                table: "ShoppingCarts",
+                column: "ProductId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ShoppingCarts_AspNetUsers_ApplicationUserId",
+                table: "ShoppingCarts",
+                column: "ApplicationUserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ShoppingCarts_Products_ProductId",
+                table: "ShoppingCarts",
+                column: "ProductId",
+                principalTable: "Products",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ShoppingCarts_AspNetUsers_ApplicationUserId",
+                table: "ShoppingCarts");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_ShoppingCarts_Products_ProductId",
+                table: "ShoppingCarts");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ShoppingCarts_ApplicationUserId",
+                table: "ShoppingCarts");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ShoppingCarts_ProductId",
+                table: "ShoppingCarts");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ApplicationUserId",
+                table: "ShoppingCarts",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedDateTime",
+                value: new DateTime(2023, 12, 24, 18, 52, 51, 168, DateTimeKind.Local).AddTicks(1066));
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedDateTime",
+                value: new DateTime(2023, 12, 24, 18, 52, 51, 168, DateTimeKind.Local).AddTicks(1118));
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreatedDateTime",
+                value: new DateTime(2023, 12, 24, 18, 52, 51, 168, DateTimeKind.Local).AddTicks(1119));
+        }
+    }
+}
--- WebProject.Models/ShoppingCart.cs
+++ WebProject.Models/ShoppingCart.cs
@@ -12,13 +12,18 @@ namespace WebProject.Models
     public class ShoppingCart
     {
         public int Id { get; set; }
+
+        public int ProductId { get; set; }
         [ForeignKey("ProductId")]
         [ValidateNever]
-        public int ProductId { get; set; }
+        public Product Product { get; set; }
+
         [ForeignKey("ApplicationUserId")]
         [ValidateNever]
+        public ApplicationUser ApplicationUser { get; set; }
         public string ApplicationUserId { get; set; }
-        [Range(1,1000, ErrorMessage = "Please enter a value between 1 and 1000")]
+
+        [Range(1, 1000, ErrorMessage = "Please enter a value between 1 and 1000")]
         public int Count { get; set; }
     }
 }
