c6d0cd2dc2d0e45a5ed33775dc92898fed97c411
LUKE,OLIVER,JOHN ATKINSON <lukeatkinson3@gmail.com>
2023-12-30 21:03:15 +0000

    Changing Order Header Columns & Model Property Names

---
+++ WebProject.DataAccess/Migrations/20231230210221_ChangeOrderDetailHeaderNames.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebProject.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeOrderDetailHeaderNames : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "State",
+                table: "OrderHeaders",
+                newName: "PostCode");
+
+            migrationBuilder.RenameColumn(
+                name: "PostalCode",
+                table: "OrderHeaders",
+                newName: "County");
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedDateTime",
+                value: new DateTime(2023, 12, 30, 21, 2, 20, 340, DateTimeKind.Local).AddTicks(9986));
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedDateTime",
+                value: new DateTime(2023, 12, 30, 21, 2, 20, 341, DateTimeKind.Local).AddTicks(38));
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreatedDateTime",
+                value: new DateTime(2023, 12, 30, 21, 2, 20, 341, DateTimeKind.Local).AddTicks(40));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "PostCode",
+                table: "OrderHeaders",
+                newName: "State");
+
+            migrationBuilder.RenameColumn(
+                name: "County",
+                table: "OrderHeaders",
+                newName: "PostalCode");
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedDateTime",
+                value: new DateTime(2023, 12, 30, 20, 36, 49, 604, DateTimeKind.Local).AddTicks(759));
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedDateTime",
+                value: new DateTime(2023, 12, 30, 20, 36, 49, 604, DateTimeKind.Local).AddTicks(812));
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreatedDateTime",
+                value: new DateTime(2023, 12, 30, 20, 36, 49, 604, DateTimeKind.Local).AddTicks(813));
+        }
+    }
+}
