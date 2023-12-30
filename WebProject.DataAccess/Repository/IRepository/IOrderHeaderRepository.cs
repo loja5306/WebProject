999b3607021058049c89d8f628ae5977a4d6c486
LUKE,OLIVER,JOHN ATKINSON <lukeatkinson3@gmail.com>
2023-12-30 20:45:45 +0000

    Adding Order Header & Details Models, Tables & Repositories

---
+++ WebProject.DataAccess/Repository/IRepository/IOrderHeaderRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebProject.Models;
+
+namespace WebProject.DataAccess.Repository.IRepository
+{
+    public interface IOrderHeaderRepository : IRepository<OrderDetail>
+    {
+        void Update(OrderDetail orderDetail);
+    }
+}
---
+++ WebProject.DataAccess/Repository/OrderHeaderRepository.cs
@@ -0,0 +1,19 @@
+﻿using WebProject.DataAccess.Repository.IRepository;
+using WebProject.Models;
+
+namespace WebProject.DataAccess.Repository
+{
+    public class OrderHeaderRepository : Repository<OrderHeader>, IOrderHeaderRepository
+    {
+        private ApplicationDbContext _db;
+        public OrderHeaderRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(OrderHeader orderHeader)
+        {
+            _db.OrderHeaders.Update(orderHeader);
+        }
+    }
+}
